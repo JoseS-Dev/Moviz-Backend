b9698b4d2d7c46574508ed7ec29c326cd448af89
JoseS-Dev <joseasantana05@gmail.com>
2026-05-03 14:52:09 -0400

    feat: :refactor: Refactor SessionModule to remove middleware and include SessionGuard for improved session management

--- api/src/modules/control/session/session.module.ts
+++ api/src/modules/control/session/session.module.ts
@@ -1,11 +1,11 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { SessionService } from './session.service.js';
 import { SessionController } from './session.controller.js';
 import { PrismaService } from '../../../prisma.service.js'
 import { JwtModule } from '@nestjs/jwt';
 import { settings } from '../../../../config/settings.config.js';
-import { SessionAuthenticationMiddleware } from './guards/session.guards.js';
-import { RequestMethod } from '@nestjs/common';
+import { SessionGuard } from './guards/session.guards.js';
+
 
 @Module({
   imports: [
@@ -17,16 +17,7 @@ import { RequestMethod } from '@nestjs/common';
     })
   ],
   controllers: [SessionController],
-  providers: [SessionService, PrismaService],
+  providers: [SessionService, PrismaService, SessionGuard],
 })
 
-export class SessionModule {
-  configure(consumer: MiddlewareConsumer){
-    consumer
-    .apply(SessionAuthenticationMiddleware)
-    .forRoutes(
-      { path: 'session/logout', method: RequestMethod.POST },
-      { path: 'session/verify', method: RequestMethod.GET }
-    )
-  }
-}
+export class SessionModule {}
