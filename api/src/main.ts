2f1830bcbdd91ad922faa2a0a3a42c984ed06afe
JoseS-Dev <joseasantana05@gmail.com>
2026-05-03 14:49:26 -0400

    feat: :sparkles: Enhance bootstrap function with global filters and pipes for improved request handling

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DefaultValuePipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter.js';
 import { settings } from '../config/settings.config.js';
 import morgan from 'morgan';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
   
   app.setGlobalPrefix(settings.basePath)
+  app.useGlobalFilters(new AllExceptionsFilter())
+  app.useGlobalPipes(new DefaultValuePipe({
+    whiteList: true,
+    transform: true
+  }))
 
   app.enableCors({
     origin: settings.corsOrigin,
